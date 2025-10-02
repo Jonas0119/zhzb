74b1843377c2e31a13b032fd21c518ddacc76532
Jonas0119 <jonaske@126.com>
2025-10-02 15:47:44 +0800

    change for Speed Insights

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <router-view />
+    <SpeedInsights />
   </div>
 </template>
 
 <script setup>
 // 应用根组件
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <style scoped>
